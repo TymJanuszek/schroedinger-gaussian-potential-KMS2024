08c878bb3d9159a68c3393ebda88aa104408d7f3
TJanuszek <mrocznyplaygames@gmail.com>
2024-11-23 00:29:39 +0100

    v1.1 GIFs
    Can now save animations as GIFs

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import schrodinger
-import numpy as np
+
+func = schrodinger.WaveFunc(500, 0.002, 0.5, 10, -5, 25)
+func.psi2_simulate_and_save(1, "psi2_k=0.5,V=0.gif", 0)
+func.psi2_simulate_and_save(1, "psi2_k=0.5,V=gaus.gif", 1)
+
+func.psi_simulate_and_save(1, "psi_k=0.5,V=0.gif", 0)
+func.psi_simulate_and_save(1, "psi_k=0.5,V=gaus.gif", 1)
+
+func = schrodinger.WaveFunc(500, 0.002, 0.75, 10, -5, 25)
+func.psi2_simulate_and_save(1, "psi2_k=0.75,V=0.gif", 0)
+func.psi2_simulate_and_save(1, "psi2_k=0.75,V=gaus.gif", 1)
+
+func.psi_simulate_and_save(1, "psi_k=0.75,V=0.gif", 0)
+func.psi_simulate_and_save(1, "psi_k=0.75,V=gaus.gif", 1)
+
+func = schrodinger.WaveFunc(500, 0.002, 1, 10, -5, 25)
+func.psi2_simulate_and_save(1, "psi2_k=1,V=0.gif", 0)
+func.psi2_simulate_and_save(1, "psi2_k=1,V=gaus.gif", 1)
+
+func.psi_simulate_and_save(1, "psi_k=1,V=0.gif", 0)
+func.psi_simulate_and_save(1, "psi_k=1,V=gaus.gif", 1)
 
 func = schrodinger.WaveFunc(500, 0.002, 1.5, 10, -5, 25)
+func.psi2_simulate_and_save(1, "psi2_k=1.5,V=0.gif", 0)
+func.psi2_simulate_and_save(1, "psi2_k=1.5,V=gaus.gif", 1)
 
-func.simulate(10, 10000)
-
+func.psi_simulate_and_save(1, "psi_k=1,V=0.gif", 0)
+func.psi_simulate_and_save(1, "psi_k=1,V=gaus.gif", 1)
--- schrodinger.py
+++ schrodinger.py
@@ -1,11 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import scipy.sparse.linalg as sc
+import io
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 
 class WaveFunc:
 
-    def __init__(self, x_steps, dt, dk, k0, minbound, maxbound, v0 = 105, sigma = 0.5):
+    def __init__(self, x_steps, dt, dk, k0, minbound, maxbound, v0=105, sigma=0.5):
 
         self.xsteps = x_steps
         self.dx = (maxbound - minbound) / x_steps
@@ -37,28 +39,117 @@ class WaveFunc:
             A[i, i] = 1 + 1j * self.dt / 2 * (2 / self.dx ** 2 + self.V[i])
         return A
 
-    def count_right_side(self):
+    def count_right_side(self, n):
         rside = 1j * np.zeros(self.xsteps + 1)
         for i in range(1, self.xsteps - 1):
             rside[i] = self.psi[i] + 1j * self.dt / 2 * (
-                        (self.psi[i + 1] - 2 * self.psi[i] + self.psi[i - 1]) / np.power(self.dx, 2) - self.V[i] *
-                        self.psi[i])
+                    (self.psi[i + 1] - 2 * self.psi[i] + self.psi[i - 1]) / np.power(self.dx, 2) - n*self.V[i] *
+                    self.psi[i])
         return rside
 
-    def simulate(self, duration, speed):
+    def psi2_simulate(self, duration, speed, Vstatus):
         self.count_psi_0()
         A = self.count_A()
 
         rng = int(duration / self.dt)
         end = len(self.psi) - 1
+
+        fig = plt.figure(figsize=(15, 9))
+
         for i in range(rng):
-            rside = self.count_right_side()
+            rside = self.count_right_side(Vstatus)
             self.psi[1:end] = sc.spsolve(A, rside[1:end])
 
-            # print(self.dx*np.sum(np.real(self.psi) ** 2 + np.imag(self.psi) ** 2))
-
-            plt.plot(self.X, np.real(self.psi) ** 2 + np.imag(self.psi) ** 2)
-            plt.plot(self.X, self.V / self.V0)
+            plt.plot(self.X, Vstatus*self.V / self.V0, "g--", label=("Potential V(x)/"+str(self.V0)))
+            plt.plot(self.X, np.real(self.psi) ** 2 + np.imag(self.psi) ** 2, "b", label=r"$|\psi(x,t)|^2$")
+            plt.legend()
+            plt.legend()
             plt.draw()
-            plt.pause(1/speed)
+            plt.pause(1 / speed)
             plt.clf()
+
+    def psi2_simulate_and_save(self, duration, filename, Vstatus):
+        self.count_psi_0()
+        A = self.count_A()
+
+        rng = int(duration / self.dt)
+        end = len(self.psi) - 1
+
+        fig = plt.figure(figsize=(15, 9))
+        frames = []
+        plt.title(filename)
+
+        for i in range(rng):
+            rside = self.count_right_side(Vstatus)
+            self.psi[1:end] = sc.spsolve(A, rside[1:end])
+
+            plt.clf()
+            plt.plot(self.X, Vstatus*self.V / self.V0, "g--", label=("Potential V(x)/" + str(self.V0)))
+            plt.plot(self.X, np.real(self.psi) ** 2 + np.imag(self.psi) ** 2, "b", label=r"$|\psi(x,t)|^2$")
+            plt.legend()
+            buf = io.BytesIO()
+            plt.savefig(buf, format="png")
+            buf.seek(0)
+            frames.append(Image.open(buf))
+
+        frames[0].save(
+            filename,
+            save_all=True,
+            append_images=frames[1:],
+            duration=200,
+            loop=0,
+        )
+
+    def psi_simulate(self, duration, speed, Vstatus):
+        self.count_psi_0()
+        A = self.count_A()
+
+        rng = int(duration / self.dt)
+        end = len(self.psi) - 1
+
+        fig = plt.figure(figsize=(15, 9))
+
+        for i in range(rng):
+            rside = self.count_right_side(Vstatus)
+            self.psi[1:end] = sc.spsolve(A, rside[1:end])
+
+            plt.plot(self.X, Vstatus * self.V / self.V0, "g--", label=("Potential V(x)/" + str(self.V0)))
+            plt.plot(self.X, np.imag(self.psi), "r", label=r"$Re(\psi(x,t))")
+            plt.plot(self.X, np.real(self.psi), "b", label=r"$Im(\psi(x,t))$")
+            plt.legend()
+            plt.draw()
+            plt.pause(1 / speed)
+            plt.clf()
+
+    def psi_simulate_and_save(self, duration, filename, Vstatus):
+        self.count_psi_0()
+        A = self.count_A()
+
+        rng = int(duration / self.dt)
+        end = len(self.psi) - 1
+
+        fig = plt.figure(figsize=(15, 9))
+        frames = []
+        plt.title(filename)
+
+        for i in range(rng):
+            rside = self.count_right_side(Vstatus)
+            self.psi[1:end] = sc.spsolve(A, rside[1:end])
+
+            plt.clf()
+            plt.plot(self.X, Vstatus*self.V / self.V0, "g--", label=("Potential V(x)/" + str(self.V0)))
+            plt.plot(self.X, np.imag(self.psi), "r", label=r"$Re(\psi(x,t))")
+            plt.plot(self.X, np.real(self.psi), "b", label=r"$Im(\psi(x,t))$")
+            plt.legend()
+            buf = io.BytesIO()
+            plt.savefig(buf, format="png")
+            buf.seek(0)
+            frames.append(Image.open(buf))
+
+        frames[0].save(
+            filename,
+            save_all=True,
+            append_images=frames[1:],
+            duration=200,
+            loop=0,
+        )
